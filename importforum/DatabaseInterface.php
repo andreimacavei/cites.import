47ebc958ceb47c362531539fcd0fde9f4dc57afd
Andrei-Marius Dincu <andrei.dincu@gmail.com>
2013-07-06 16:30:26 +0300

    Added import forum sources

---
+++ importforum/DatabaseInterface.php
@@ -0,0 +1,83 @@
+<?php
+
+/**
+ * Class as an interface with the cites forum database
+ */
+class DatabaseInterface {
+  private static $dbObj = null;
+  
+  private function __construct($host,$username,$password,$database) {
+    mysql_connect($host, $username, $password) or die('Error connecting to the database server \n');
+    mysql_select_db($database) or die('Error connecting to the database');
+  }
+  
+  public static function getInstance($host,$username,$password,$database){
+    if (DatabaseInterface::$dbObj == null)
+      DatabaseInterface::$dbObj = new DatabaseInterface($host,$username,$password,$database);
+    return DatabaseInterface::$dbObj;
+  }
+  
+  
+  public function getForums(){
+    $query = "SELECT DISTINCT * 
+              FROM `groups`
+              ";
+    $result = mysql_query($query);
+    return $result;
+  }
+  
+  public function getTopics($forumId){
+    $query = "SELECT DISTINCT * 
+              FROM `categs`
+              WHERE `group_id` = '$forumId'
+              ";
+    $result = mysql_query($query);
+    return $result;
+  }
+  
+  public function getPosts($topicId){
+    $query = "SELECT DISTINCT * 
+              FROM `posts`
+              WHERE `categ_id` = $topicId
+              ORDER BY `data` ASC
+              ";
+    $result = mysql_query($query);
+    return $result;
+  }
+  
+  public function getPostsFiles($postId){
+    $query = "SELECT DISTINCT * 
+              FROM `posts_files`
+              WHERE `post_id` = $postId";
+    $result = mysql_query($query);
+    return $result;
+  }
+  
+  public function getUsers(){
+    $query = "SELECT DISTINCT *
+              FROM `users`
+              LIMIT 1";
+    $result = mysql_query($query);
+    return $result;
+  }
+  
+  public function getUser($userId){
+    $query = "SELECT * 
+              FROM `users`
+              WHERE `user_id` = $userId";
+    $result = mysql_query($query);
+    return mysql_fetch_object($result);
+  }
+  
+  public function existsFile($uri){
+    $query = "SELECT * 
+              FROM `cites5`.`file_managed`
+              WHERE 'uri' = $uri";
+    $result = mysql_query($query);
+    if (is_null(mysql_fetch_object($result)))
+      return FALSE;
+    return TRUE;
+  }
+}
+
+?>
