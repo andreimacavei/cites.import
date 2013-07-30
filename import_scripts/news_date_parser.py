0853f76d1ebac2eaf7f80e8c9276368060f210fb
andreimacavei <andrei_gabriel.macavei@cti.pub.ro>
2013-07-30 15:54:43 +0300

    Made script to also accept an input json file with urls and language asociated and also made it to append when single url are passed as input

--- import_scripts/news_date_parser.py
+++ import_scripts/news_date_parser.py
@@ -13,34 +13,59 @@ import json
 from bs4 import BeautifulSoup
 
 misspelled_months = {
-    'fevrier': u'février', 
-    u'août': u'aout'
+    'fevrier': 'février',
+    'january': 'janvier',
+    'february': 'février',
+    'march': 'mars',
+    'april': 'avril',
+    'may': 'mai',
+    'june': 'juin',
+    'july': 'juillet',
+    'august': 'août',
+    'september': 'septembre',
+    'october': 'octobre',
+    'november': 'novembre',
+    'december': 'décembre',
     }
 
-def clean_date(local_date):
+locale_dict = {
+    'eng': 'en_US.UTF-8',
+    'fra': 'fr_FR.UTF-8',
+    'esp': 'es_ES.UTF-8',
+    }
+
+def clean_date(local_date, language):
     for k, v in misspelled_months.items():
-        if local_date.lower().find(k) != -1:
+        if local_date.lower().find(k) != -1 and language == 'fra':
             local_date = local_date.lower().replace(k, v)
+        elif language == 'eng' and local_date.lower().find(v) != -1:
+            local_date = local_date.lower().replace(v, k)
 
     buf = re.sub(r"\r?\n? de", "", local_date)
-    return re.sub(" +", " ", buf.strip())
+    buf = re.sub(" +", " ", buf.strip())
+    return buf.replace("\xc2\xa0", " ")
+
 
 def get_parent_directory(url):
     parent_url = '/'.join(url.split('/')[:-1]) + '/'
     return parent_url
 
-def convert_to_mysql_date(local_date, curr_locale):
-    locale.setlocale(locale.LC_ALL, curr_locale)
+def convert_to_mysql_date(local_date, language):
+    locale.setlocale(locale.LC_ALL, locale_dict[language])
     try:
         date = datetime.strptime(local_date, '%d %B %Y')
     except ValueError:
         try:
             date = datetime.strptime(local_date, '%B %Y')
-        except:
-            return
+        except ValueError:
+            local_date = local_date.split()[-1]
+            if any(char.isdigit() for char in local_date):
+                date = datetime.strptime(local_date, '%Y')
+            else:
+                return
     return date.strftime('%Y-%m-%d')
 
-def download(url, current_locale):
+def download(url, language):
     html = urlopen(url).read()
     soup = BeautifulSoup(html, "html.parser")
     table = soup.find('table')
@@ -50,34 +75,48 @@ def download(url, current_locale):
 
     results = {}
     for url, title in urls:
-        local_date = clean_date(title.split(':')[0])
+        local_date = clean_date(title.split(':')[0].encode('utf-8'), language)
         rel_url = urlsplit(url).path
         try:
-            results[rel_url] = convert_to_mysql_date(local_date.encode('utf-8'), current_locale)
+            results[rel_url] = convert_to_mysql_date(local_date, language)
         except ValueError:
             year = rel_url.split('/')[-2]
-            #import pdb; pdb.set_trace()
             if not year in local_date: 
                 local_date = local_date + ' {}'.format(year)
-            results[rel_url] = convert_to_mysql_date(local_date.encode('utf-8'), current_locale)
+            results[rel_url] = convert_to_mysql_date(local_date, language)
 
     return results
 
-def to_json(filename, results):
-    f = open(filename, "w")
+def to_json(filename, results, mode):
+    f = open(filename, mode)
     f.write(json.dumps(results, indent=4))
     f.close()
+
+def read_input(filename):
+    json_data = open(filename)
+    data = json.load(json_data)
+    json_data.close()
+    return data
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-u', '--url', default='http://www.cites.org/eng/news/pr/index.php', help="absolute path")
-    parser.add_argument('-l', '--locale', default='en_US.UTF-8', help="example for english: 'en_US.UTF-8'")
+    parser.add_argument('-i', '--input', help="json file with urls and language")
+    parser.add_argument('-u', '--url')
+    parser.add_argument('-l', '--language')
     parser.add_argument('-o', '--output', default='news.json', help="output file where to save the data")
     args = parser.parse_args()
-    
-    
-    news = download(args.url, args.locale)
-    to_json(args.output, news)
 
+    news = {}
+    write_mode = 'w'
+
+    if args.input:
+        input_data = read_input(args.input)
+        for url, lang in input_data.items():
+            news.update(download(url, lang))
+    elif args.url:
+        write_mode = 'a'
+        news = download(args.url, args.language)
+
+    to_json(args.output, news, write_mode)
     print "{} records inserted".format(len(news))
